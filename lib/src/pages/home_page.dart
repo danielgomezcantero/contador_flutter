28810e5fd447ce0cea06cb26d7356a95d3f8f9f3
Daniel Maximiliano Gómez Cantero <danielgomezcantero@gmail.com>
2020-07-28 14:08:12 -0400

    Aplicación paso a paso Stlw Stlf

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final estiloTexto = new TextStyle(fontSize: 25.0);
+  final conteo = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
